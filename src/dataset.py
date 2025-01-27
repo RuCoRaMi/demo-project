0d692dbf7fcc3f5473b6a4dbd91610da724f6610
RuCoRaMi <rui.rakamilanisti@gmail.com>
2025-01-27 22:09:00 +0700

    new

---
+++ src/dataset.py
@@ -0,0 +1,25 @@
+def add(a, b):
+    return a + b
+
+
+def substract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return None
+    return a / b
+
+
+a = 10
+b = 5
+
+print(add(a, b))
+print(substract(a, b))
+print(multiply(a, b))
+print(divide(a, b))
